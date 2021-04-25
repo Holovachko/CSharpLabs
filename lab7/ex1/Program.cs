149f1e511926ca512e5dafc44ff1bd2dd5a42f42
Holovachko <74302993+Holovachko@users.noreply.github.com>
2021-04-25 21:41:53 +0300

    Add files via upload

---
+++ lab7/ex1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length;
+            int height;
+            int sum = 0; 
+            Console.Write("Введите довжину матриці: ");
+            length = int.Parse(Console.ReadLine());
+            Console.Write("Введите висоту матриці: ");
+            height = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nЗаповніть матрицю\n");
+            int[,] ara = new int[length, height];
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Console.Write("Введите число: ");
+                    ara[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Console.Write("{0} ", ara[i, j]);
+                    if (i%2 == 0 && j%2 == 0) {
+                        sum += ara[i, j];
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\nСумма парних чисел матриці = {0}", sum);
+        }
+    }
+}
---
+++ lab7/ex3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] matrix = new double[2, 2];
+            double det = (matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]);
+        }
+    }
+}
